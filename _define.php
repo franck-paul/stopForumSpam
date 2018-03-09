bc2646ca6f0cd975e795aa39aad0e0acb3366dcd
franck <carnet.franck.paul@gmail.com>
2018-03-09 13:59:39 +0100

    Various code formatting

--- _define.php
+++ _define.php
@@ -1,25 +1,26 @@
 <?php
-# -- BEGIN LICENSE BLOCK ----------------------------------
-# This file is part of stopforumspam, a plugin for Dotclear 2.
-#
-# Copyright (c) Franck Paul and contributors
-# carnet.franck.paul@gmail.com
-#
-# Licensed under the GPL version 2.0 license.
-# A copy of this license is available in LICENSE file or at
-# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
-# -- END LICENSE BLOCK ------------------------------------
+/**
+ * @brief stopforumspam, an antispam filter plugin for Dotclear 2
+ *
+ * @package Dotclear
+ * @subpackage Plugins
+ *
+ * @author Franck Paul
+ *
+ * @copyright Franck Paul carnet.franck.paul@gmail.com
+ * @copyright GPL-2.0 https://www.gnu.org/licenses/gpl-2.0.html
+ */
 
-if (!defined('DC_RC_PATH')) { return; }
+if (!defined('DC_RC_PATH')) {return;}
 
 $this->registerModule(
-	/* Name */			"Stop Forum Spam",
-	/* Description*/		"Stop Forum Spam filter for antispam Dotclear plugin",
-	/* Author */			"Franck Paul",
-	/* Version */			'0.2',
-	array(
-		'permissions' =>	'usage,contentadmin',
-		'priority' =>		200,
-		'type'		=>		'plugin'
-	)
+    "Stop Forum Spam",                                     // Name
+    "Stop Forum Spam filter for antispam Dotclear plugin", // Description
+    "Franck Paul",                                         // Author
+    '0.2',                                                 // Version
+    array(
+        'permissions' => 'usage,contentadmin',
+        'priority'    => 200,
+        'type'        => 'plugin'
+    )
 );
--- _prepend.php
+++ _prepend.php
@@ -1,16 +1,17 @@
 <?php
-# -- BEGIN LICENSE BLOCK ----------------------------------
-# This file is part of stopforumspam, a plugin for Dotclear 2.
-#
-# Copyright (c) Franck Paul and contributors
-# carnet.franck.paul@gmail.com
-#
-# Licensed under the GPL version 2.0 license.
-# A copy of this license is available in LICENSE file or at
-# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
-# -- END LICENSE BLOCK ------------------------------------
+/**
+ * @brief stopforumspam, an antispam filter plugin for Dotclear 2
+ *
+ * @package Dotclear
+ * @subpackage Plugins
+ *
+ * @author Franck Paul
+ *
+ * @copyright Franck Paul carnet.franck.paul@gmail.com
+ * @copyright GPL-2.0 https://www.gnu.org/licenses/gpl-2.0.html
+ */
 
-if (!defined('DC_RC_PATH')) { return; }
+if (!defined('DC_RC_PATH')) {return;}
 
-$__autoload['dcFilterStopForumSpam'] = dirname(__FILE__).'/class.dc.filter.stopforumspam.php';
-$core->spamfilters[] = 'dcFilterStopForumSpam';
+$__autoload['dcFilterStopForumSpam'] = dirname(__FILE__) . '/class.dc.filter.stopforumspam.php';
+$core->spamfilters[]                 = 'dcFilterStopForumSpam';
