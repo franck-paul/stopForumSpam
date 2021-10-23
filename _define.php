2c75b1de5fb7c64aa7dc42b179be4f8c54d9b583
franck <carnet.franck.paul@gmail.com>
2021-10-23 12:44:53 +0200

    Add alternative repository

--- _define.php
+++ _define.php
@@ -10,20 +10,23 @@
  * @copyright Franck Paul carnet.franck.paul@gmail.com
  * @copyright GPL-2.0 https://www.gnu.org/licenses/gpl-2.0.html
  */
-
-if (!defined('DC_RC_PATH')) {return;}
+if (!defined('DC_RC_PATH')) {
+    return;
+}
 
 $this->registerModule(
-    "Stop Forum Spam",                                     // Name
-    "Stop Forum Spam filter for antispam Dotclear plugin", // Description
-    "Franck Paul",                                         // Author
+    'Stop Forum Spam',                                     // Name
+    'Stop Forum Spam filter for antispam Dotclear plugin', // Description
+    'Franck Paul',                                         // Author
     '0.3',                                                 // Version
     [
         'requires'    => [['core', '2.13']], // Dependencies
         'permissions' => 'usage,contentadmin',
         'priority'    => 200,
-        'details'     => 'https://open-time.net/?q=stopForumSpam',       // Details URL
-        'support'     => 'https://github.com/franck-paul/stopForumSpam', // Support URL
-        'type'        => 'plugin'
+        'type'        => 'plugin',
+
+        'details'    => 'https://open-time.net/?q=stopForumSpam',       // Details URL
+        'support'    => 'https://github.com/franck-paul/stopForumSpam', // Support URL
+        'repository' => 'https://raw.githubusercontent.com/franck-paul/stopForumSpam/main/dcstore.xml'
     ]
 );
