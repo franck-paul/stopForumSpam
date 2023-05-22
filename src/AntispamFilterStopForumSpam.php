16688696c9e452b5415784c56fcfedd32e673b8b
franck <carnet.franck.paul@gmail.com>
2023-05-22 11:56:44 +0200

    Code review

--- src/AntispamFilterStopForumSpam.php
+++ src/AntispamFilterStopForumSpam.php
@@ -20,16 +20,34 @@ use Exception;
 
 class AntispamFilterStopForumSpam extends SpamFilter
 {
-    public $name    = 'Stop Forum Spam';
-    public $has_gui = false;
-    public $active  = false;
+    /** @var string Filter name */
+    public $name = 'Stop Forum Spam';
 
+    /** @var bool Filter has settings GUI? */
+    public $has_gui = false;
+
+    /** @var bool Is filter active? */
+    public $active = false;
+
+    /**
+     * Sets the filter description.
+     */
     protected function setInfo()
     {
         $this->description = __('Stop Forum Spam spam filter (see http://www.stopforumspam.com/)');
     }
 
-    public function getStatusMessage($status, $comment_id)
+    /**
+     * This method returns filter status message. You can overload this method to
+     * return a custom message. Message is shown in comment details and in
+     * comments list.
+     *
+     * @param      string  $status      The status
+     * @param      int     $comment_id  The comment identifier
+     *
+     * @return     string  The status message.
+     */
+    public function getStatusMessage(string $status, ?int $comment_id)
     {
         return sprintf(__('Filtered by %s.'), $this->guiLink());
     }
@@ -39,7 +57,24 @@ class AntispamFilterStopForumSpam extends SpamFilter
         return new StopForumSpam(dcCore::app()->blog->url);
     }
 
-    public function isSpam($type, $author, $email, $site, $ip, $content, $post_id, &$status)
+    /**
+     * This method should return if a comment is a spam or not. If it returns true
+     * or false, execution of next filters will be stoped. If should return nothing
+     * to let next filters apply.
+     *
+     * Your filter should also fill $status variable with its own information if
+     * comment is a spam.
+     *
+     * @param      string  $type     The comment type (comment / trackback)
+     * @param      string  $author   The comment author
+     * @param      string  $email    The comment author email
+     * @param      string  $site     The comment author site
+     * @param      string  $ip       The comment author IP
+     * @param      string  $content  The comment content
+     * @param      int     $post_id  The comment post_id
+     * @param      string  $status   The comment status
+     */
+    public function isSpam(string $type, ?string $author, ?string $email, ?string $site, ?string $ip, ?string $content, ?int $post_id, string &$status)
     {
         if (($sfs = $this->sfsInit()) === false) {
             return;
