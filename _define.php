ffcb01ebb92c36833617f3f83c45bd797a7cdb9c
franck <carnet.franck.paul@gmail.com>
2023-10-24 19:39:44 +0200

    New release 4.0

--- _define.php
+++ _define.php
@@ -14,7 +14,7 @@ $this->registerModule(
     'Stop Forum Spam',
     'Stop Forum Spam filter for antispam Dotclear plugin',
     'Franck Paul',
-    '3.0',
+    '4.0',
     [
         'requires'    => [['core', '2.28']],
         'permissions' => 'My',
